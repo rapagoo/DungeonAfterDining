a374398160aafc9458610f4a569269705a4d2897
Sungrok Hong <lilspicypepper@gmail.com>
2025-04-16 00:49:40 +0900

    파쿠르 시스템 합치기 시도

---
+++ Source/Dungeon/ClimbingSystem.Build.cs
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ClimbingSystem : ModuleRules
+{
+	public ClimbingSystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+            "MotionWarping"});
+	}
+}
